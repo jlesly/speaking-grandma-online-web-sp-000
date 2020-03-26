6d7233bcf03a88c9f9760455393791a3318a42c9
Lesly Juarez <leslyj2@gmail.com>
2020-03-26 12:31:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,8 +8,7 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
-    # NO, NOT SINCE 1938!
-    else
+    else phrase == 
   		return "NO, NOT SINCE 1938!"
     end
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
