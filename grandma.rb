e7d4e61bca63b5bf7e4e9ba0ef2857d22f86efcb
Lesly Juarez <leslyj2@gmail.com>
2020-03-26 12:23:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ def speak_to_grandma(phrase)
   		return "HUH?! SPEAK UP, SONNY!"
     elsif phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-    else phrase.upcase
+    elsif phrase.upcase
       return "NO, NOT SINCE 1938!"
     # NO, NOT SINCE 1938!
     else
