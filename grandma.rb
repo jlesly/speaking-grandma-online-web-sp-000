ef51c9bbd2e46eacf990947ef0e0b4b4a0ae2fc4
Lesly Juarez <leslyj2@gmail.com>
2020-03-26 12:11:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(phrase)
-if phrase != phrase.upcase
-  puts "HUH?! SPEAK UP, SONNY!"
+if phrase == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+  
   elsif phrase.upcase
   puts "NO, NOT SINCE 1938!"
 else phrase.upcase == "I LOVE YOU GRANDMA!"
